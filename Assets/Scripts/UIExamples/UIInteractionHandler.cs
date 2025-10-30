4dcd0ca754264a403f7a7c5f88589f4e57ea745e
Edoardo Del Sarto <e99delsarto@gmail.com>
2025-10-30 09:38:46 +0100

    Add UIInteractionHandler script for handling UI events
    - Implemented interfaces for pointer events: IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler, IDragHandler, ISelectHandler, IDeselectHandler.
    - Added debug logs for each event to track interactions with UI elements.
    - Created associated .meta file for the new script.

---
+++ Assets/Scripts/UIExamples/UIInteractionHandler.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+// Per ricevere eventi su un oggetto UI, questo script deve essere associato a un GameObject
+// che ha anche un componente grafico (es. Image, Text, Button) con "Raycast Target" abilitato.
+public class UIInteractionHandler : MonoBehaviour, 
+    IPointerEnterHandler, 
+    IPointerExitHandler, 
+    IPointerDownHandler, 
+    IPointerUpHandler,
+    IPointerClickHandler,
+    IDragHandler,
+    ISelectHandler,
+    IDeselectHandler
+{
+    // Implementazione di IPointerClickHandler
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Oggetto cliccato (tramite interfaccia)");
+    }
+
+    // Implementazione di IPointerDownHandler
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Puntatore premuto sull'oggetto (tramite interfaccia)");
+    }
+
+    // Implementazione di IPointerUpHandler
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("Puntatore rilasciato sull'oggetto (tramite interfaccia)");
+    }
+
+    // Implementazione di IPointerEnterHandler
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Puntatore entrato nell'area dell'oggetto (tramite interfaccia)");
+    }
+
+    // Implementazione di IPointerExitHandler
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Puntatore uscito dall'area dell'oggetto (tramite interfaccia)");
+    }
+
+    // Implementazione di IDragHandler
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log($"Trascinamento in corso: {eventData.delta} (tramite interfaccia)");
+    }
+
+    // Implementazione di ISelectHandler
+    public void OnSelect(BaseEventData eventData)
+    {
+        Debug.Log("Oggetto selezionato (tramite interfaccia)");
+    }
+
+    // Implementazione di IDeselectHandler
+    public void OnDeselect(BaseEventData eventData)
+    {
+        Debug.Log("Oggetto deselezionato (tramite interfaccia)");
+    }
+}
--- Assets/Scripts/UIExamples/UIManager.cs
+++ Assets/Scripts/UIExamples/UIManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class UIManager : MonoBehaviour
 {
@@ -9,8 +10,62 @@ public class UIManager : MonoBehaviour
 
     void OnEnable()
     {
+        // Eventi Button standard
         _button.onClick.AddListener(() => Interazione(messaggioPersonalizzato));
         _button.onClick.AddListener(InterazioneSemplice);
+
+        // Altri eventi tramite EventTrigger
+        AggiungiEventiAggiuntivi();
+    }
+    
+    void AggiungiEventiAggiuntivi()
+    {
+        // Ottieni o aggiungi EventTrigger component
+        EventTrigger trigger = _button.GetComponent<EventTrigger>();
+        if (trigger == null)
+            trigger = _button.gameObject.AddComponent<EventTrigger>();
+        
+        // Pointer Enter (mouse hover)
+        EventTrigger.Entry entryEnter = new EventTrigger.Entry();
+        entryEnter.eventID = EventTriggerType.PointerEnter;
+        entryEnter.callback.AddListener((data) => { OnPointerEnter((PointerEventData)data); });
+        trigger.triggers.Add(entryEnter);
+        
+        // Pointer Exit (mouse leave)
+        EventTrigger.Entry entryExit = new EventTrigger.Entry();
+        entryExit.eventID = EventTriggerType.PointerExit;
+        entryExit.callback.AddListener((data) => { OnPointerExit((PointerEventData)data); });
+        trigger.triggers.Add(entryExit);
+        
+        // Pointer Down (mouse press down)
+        EventTrigger.Entry entryDown = new EventTrigger.Entry();
+        entryDown.eventID = EventTriggerType.PointerDown;
+        entryDown.callback.AddListener((data) => { OnPointerDown((PointerEventData)data); });
+        trigger.triggers.Add(entryDown);
+        
+        // Pointer Up (mouse release)
+        EventTrigger.Entry entryUp = new EventTrigger.Entry();
+        entryUp.eventID = EventTriggerType.PointerUp;
+        entryUp.callback.AddListener((data) => { OnPointerUp((PointerEventData)data); });
+        trigger.triggers.Add(entryUp);
+        
+        // Drag (trascinamento)
+        EventTrigger.Entry entryDrag = new EventTrigger.Entry();
+        entryDrag.eventID = EventTriggerType.Drag;
+        entryDrag.callback.AddListener((data) => { OnDrag((PointerEventData)data); });
+        trigger.triggers.Add(entryDrag);
+
+        // Select (quando l'oggetto viene selezionato tramite navigazione)
+        EventTrigger.Entry entrySelect = new EventTrigger.Entry();
+        entrySelect.eventID = EventTriggerType.Select;
+        entrySelect.callback.AddListener((data) => { OnSelect(data); });
+        trigger.triggers.Add(entrySelect);
+
+        // Deselect (quando l'oggetto perde la selezione)
+        EventTrigger.Entry entryDeselect = new EventTrigger.Entry();
+        entryDeselect.eventID = EventTriggerType.Deselect;
+        entryDeselect.callback.AddListener((data) => { OnDeselect(data); });
+        trigger.triggers.Add(entryDeselect);
     }
 
     public void Interazione(string messaggio)
@@ -20,5 +75,49 @@ public class UIManager : MonoBehaviour
 
     public void InterazioneSemplice(){
 
+    }
+    
+    // Gestori degli eventi del mouse/touch
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Mouse entrato nel button");
+        // Esempio: cambia colore o scala
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Mouse uscito dal button");
+        // Esempio: ripristina colore o scala
+    }
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Button premuto (mouse down)");
+        // Esempio: effetto di pressione
+    }
+    
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("Button rilasciato (mouse up)");
+        // Esempio: ripristina da effetto pressione
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log($"Button trascinato: {eventData.delta}");
+        _button.transform.position += (Vector3)eventData.delta;
+    }
+    
+    // Gestori degli eventi di selezione
+    public void OnSelect(BaseEventData eventData)
+    {
+        Debug.Log("Button selezionato");
+        // Esempio: mostra un bordo di selezione per la navigazione da tastiera/gamepad
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        Debug.Log("Button deselezionato");
+        // Esempio: nascondi il bordo di selezione
     }
 }
