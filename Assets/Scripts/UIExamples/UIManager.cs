f2caaf3704465d4d496c74f10d4996bb3f0648c5
Edoardo Del Sarto <e99delsarto@gmail.com>
2025-10-30 09:53:15 +0100

    Comment out unused EventTrigger functionality in UIManager

--- Assets/Scripts/UIExamples/UIManager.cs
+++ Assets/Scripts/UIExamples/UIManager.cs
@@ -15,58 +15,58 @@ public class UIManager : MonoBehaviour
         _button.onClick.AddListener(InterazioneSemplice);
 
         // Altri eventi tramite EventTrigger
-        AggiungiEventiAggiuntivi();
+        // AggiungiEventiAggiuntivi();
     }
     
-    void AggiungiEventiAggiuntivi()
-    {
+    // void AggiungiEventiAggiuntivi()
+    // {
         // Ottieni o aggiungi EventTrigger component
-        EventTrigger trigger = _button.GetComponent<EventTrigger>();
-        if (trigger == null)
-            trigger = _button.gameObject.AddComponent<EventTrigger>();
+    //     EventTrigger trigger = _button.GetComponent<EventTrigger>();
+    //     if (trigger == null)
+    //         trigger = _button.gameObject.AddComponent<EventTrigger>();
         
-        // Pointer Enter (mouse hover)
-        EventTrigger.Entry entryEnter = new EventTrigger.Entry();
-        entryEnter.eventID = EventTriggerType.PointerEnter;
-        entryEnter.callback.AddListener((data) => { OnPointerEnter((PointerEventData)data); });
-        trigger.triggers.Add(entryEnter);
+    //     // Pointer Enter (mouse hover)
+    //     EventTrigger.Entry entryEnter = new EventTrigger.Entry();
+    //     entryEnter.eventID = EventTriggerType.PointerEnter;
+    //     entryEnter.callback.AddListener((data) => { OnPointerEnter((PointerEventData)data); });
+    //     trigger.triggers.Add(entryEnter);
         
-        // Pointer Exit (mouse leave)
-        EventTrigger.Entry entryExit = new EventTrigger.Entry();
-        entryExit.eventID = EventTriggerType.PointerExit;
-        entryExit.callback.AddListener((data) => { OnPointerExit((PointerEventData)data); });
-        trigger.triggers.Add(entryExit);
+    //     // Pointer Exit (mouse leave)
+    //     EventTrigger.Entry entryExit = new EventTrigger.Entry();
+    //     entryExit.eventID = EventTriggerType.PointerExit;
+    //     entryExit.callback.AddListener((data) => { OnPointerExit((PointerEventData)data); });
+    //     trigger.triggers.Add(entryExit);
         
-        // Pointer Down (mouse press down)
-        EventTrigger.Entry entryDown = new EventTrigger.Entry();
-        entryDown.eventID = EventTriggerType.PointerDown;
-        entryDown.callback.AddListener((data) => { OnPointerDown((PointerEventData)data); });
-        trigger.triggers.Add(entryDown);
+    //     // Pointer Down (mouse press down)
+    //     EventTrigger.Entry entryDown = new EventTrigger.Entry();
+    //     entryDown.eventID = EventTriggerType.PointerDown;
+    //     entryDown.callback.AddListener((data) => { OnPointerDown((PointerEventData)data); });
+    //     trigger.triggers.Add(entryDown);
         
-        // Pointer Up (mouse release)
-        EventTrigger.Entry entryUp = new EventTrigger.Entry();
-        entryUp.eventID = EventTriggerType.PointerUp;
-        entryUp.callback.AddListener((data) => { OnPointerUp((PointerEventData)data); });
-        trigger.triggers.Add(entryUp);
+    //     // Pointer Up (mouse release)
+    //     EventTrigger.Entry entryUp = new EventTrigger.Entry();
+    //     entryUp.eventID = EventTriggerType.PointerUp;
+    //     entryUp.callback.AddListener((data) => { OnPointerUp((PointerEventData)data); });
+    //     trigger.triggers.Add(entryUp);
         
-        // Drag (trascinamento)
-        EventTrigger.Entry entryDrag = new EventTrigger.Entry();
-        entryDrag.eventID = EventTriggerType.Drag;
-        entryDrag.callback.AddListener((data) => { OnDrag((PointerEventData)data); });
-        trigger.triggers.Add(entryDrag);
+    //     // Drag (trascinamento)
+    //     EventTrigger.Entry entryDrag = new EventTrigger.Entry();
+    //     entryDrag.eventID = EventTriggerType.Drag;
+    //     entryDrag.callback.AddListener((data) => { OnDrag((PointerEventData)data); });
+    //     trigger.triggers.Add(entryDrag);
 
-        // Select (quando l'oggetto viene selezionato tramite navigazione)
-        EventTrigger.Entry entrySelect = new EventTrigger.Entry();
-        entrySelect.eventID = EventTriggerType.Select;
-        entrySelect.callback.AddListener((data) => { OnSelect(data); });
-        trigger.triggers.Add(entrySelect);
+    //     // Select (quando l'oggetto viene selezionato tramite navigazione)
+    //     EventTrigger.Entry entrySelect = new EventTrigger.Entry();
+    //     entrySelect.eventID = EventTriggerType.Select;
+    //     entrySelect.callback.AddListener((data) => { OnSelect(data); });
+    //     trigger.triggers.Add(entrySelect);
 
-        // Deselect (quando l'oggetto perde la selezione)
-        EventTrigger.Entry entryDeselect = new EventTrigger.Entry();
-        entryDeselect.eventID = EventTriggerType.Deselect;
-        entryDeselect.callback.AddListener((data) => { OnDeselect(data); });
-        trigger.triggers.Add(entryDeselect);
-    }
+    //     // Deselect (quando l'oggetto perde la selezione)
+    //     EventTrigger.Entry entryDeselect = new EventTrigger.Entry();
+    //     entryDeselect.eventID = EventTriggerType.Deselect;
+    //     entryDeselect.callback.AddListener((data) => { OnDeselect(data); });
+    //     trigger.triggers.Add(entryDeselect);
+    // }
 
     public void Interazione(string messaggio)
     {
@@ -105,7 +105,6 @@ public class UIManager : MonoBehaviour
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log($"Button trascinato: {eventData.delta}");
-        _button.transform.position += (Vector3)eventData.delta;
     }
     
     // Gestori degli eventi di selezione
