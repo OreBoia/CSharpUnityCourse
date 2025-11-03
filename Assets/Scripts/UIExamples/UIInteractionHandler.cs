bf91ace9b71757366d6704b5462713ecd4106d08
Edoardo Del Sarto <e99delsarto@gmail.com>
2025-11-03 12:56:52 +0100

    Add drag image in limit rect

--- Assets/Scripts/UIExamples/UIInteractionHandler.cs
+++ Assets/Scripts/UIExamples/UIInteractionHandler.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 // Per ricevere eventi su un oggetto UI, questo script deve essere associato a un GameObject
 // che ha anche un componente grafico (es. Image, Text, Button) con "Raycast Target" abilitato.
-public class UIInteractionHandler : MonoBehaviour,
+public class UIInteractionHandler : Selectable,
     IPointerEnterHandler,
     IPointerExitHandler,
     IPointerDownHandler,
@@ -17,80 +17,136 @@ public class UIInteractionHandler : MonoBehaviour,
     IDeselectHandler,
     IMoveHandler
 {
+    // Limiti per il trascinamento
+    float minX;
+    float maxX;
+    float minY;
+    float maxY;
+
+    protected override void Start()
+    {
+        base.Start();
+        GetRectTransform();
+    }
+
     // Implementazione di IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
-        // Debug.Log("Oggetto cliccato (tramite interfaccia)");
+        Debug.Log("Oggetto cliccato (tramite interfaccia)");
     }
 
     // Implementazione di IPointerDownHandler
-    public void OnPointerDown(PointerEventData eventData)
+    public override void OnPointerDown(PointerEventData eventData)
     {
-        // Debug.Log("Puntatore premuto sull'oggetto (tramite interfaccia)");
-        // if(eventData.button == PointerEventData.InputButton.Left)
-        // {
-        //     Debug.Log("Clic sinistro rilevato");
-        //     Debug.Log($"Numero di clic: {eventData.clickCount}");
+        base.OnPointerDown(eventData);
+        Debug.Log("Puntatore premuto sull'oggetto (tramite interfaccia)");
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            Debug.Log("Clic sinistro rilevato");
+            Debug.Log($"Numero di clic: {eventData.clickCount}");
 
-        //     if(eventData.clickCount == 2)
-        //     {
-        //         Debug.Log("Doppio clic rilevato!");
-        //         gameObject.GetComponent<Image>().color = new Color(Random.value, Random.value, Random.value);
-        //     }
-        // }
+            if (eventData.clickCount == 2)
+            {
+                Debug.Log("Doppio clic rilevato!");
+                gameObject.GetComponent<Image>().color = new Color(Random.value, Random.value, Random.value);
+            }
+        }
     }
 
     // Implementazione di IPointerUpHandler
-    public void OnPointerUp(PointerEventData eventData)
+    public override void OnPointerUp(PointerEventData eventData)
     {
-        // Debug.Log("Puntatore rilasciato sull'oggetto (tramite interfaccia)");
+        base.OnPointerUp(eventData);
+        Debug.Log("Puntatore rilasciato sull'oggetto (tramite interfaccia)");
     }
 
     // Implementazione di IPointerEnterHandler
-    public void OnPointerEnter(PointerEventData eventData)
+    public override void OnPointerEnter(PointerEventData eventData)
     {
-        // Debug.Log("Puntatore entrato nell'area dell'oggetto (tramite interfaccia)");
+        base.OnPointerEnter(eventData);
+        Debug.Log("Puntatore entrato nell'area dell'oggetto (tramite interfaccia)");
     }
 
     // Implementazione di IPointerExitHandler
-    public void OnPointerExit(PointerEventData eventData)
+    public override void OnPointerExit(PointerEventData eventData)
     {
-        // Debug.Log("Puntatore uscito dall'area dell'oggetto (tramite interfaccia)");
+        base.OnPointerExit(eventData);
+        Debug.Log("Puntatore uscito dall'area dell'oggetto (tramite interfaccia)");
     }
 
     // Implementazione di IDragHandler
     public void OnDrag(PointerEventData eventData)
     {
-        // Debug.Log($"Trascinamento in corso: {eventData.delta} (tramite interfaccia)");
-        // transform.position += (Vector3)eventData.delta;
+        Debug.Log($"Trascinamento in corso: {eventData.delta} (tramite interfaccia)");
+        transform.position += (Vector3)eventData.delta;
+        ClampPosition();
     }
 
-    // Implementazione di ISelectHandler
+    private void ClampPosition()
+    {
+        RectTransform rt = transform as RectTransform;
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, minX + rt.rect.width / 2, maxX - rt.rect.width / 2);
+        pos.y = Mathf.Clamp(pos.y, minY + rt.rect.height / 2, maxY - rt.rect.height / 2);
+        transform.position = pos;
+    }
+
+    // Implementazione di IBeginDragHandler
     public void OnBeginDrag(PointerEventData eventData)
     {
-        // throw new System.NotImplementedException();
+        Debug.Log("Inizio trascinamento (tramite interfaccia)");
     }
 
+    // Implementazione di IEndDragHandler
     public void OnEndDrag(PointerEventData eventData)
     {
-        // throw new System.NotImplementedException();
+        Debug.Log("Fine trascinamento (tramite interfaccia)");
     }
 
-    public void OnSelect(BaseEventData eventData)
+    public override void OnSelect(BaseEventData eventData)
     {
+        base.OnSelect(eventData);
         Debug.Log("Oggetto selezionato (tramite interfaccia)");
     }
 
     // Implementazione di IDeselectHandler
-    public void OnDeselect(BaseEventData eventData)
+    public override void OnDeselect(BaseEventData eventData)
     {
+        base.OnDeselect(eventData);
         Debug.Log("Oggetto deselezionato (tramite interfaccia)");
     }
 
 
-    public void OnMove(AxisEventData eventData)
+    public override void OnMove(AxisEventData eventData)
     {
         Debug.Log($"Movimento con input direzionale: {eventData.moveDir} (tramite interfaccia)");
     }
 
+    public Image Limits;
+    private void GetRectTransform()
+    {
+        if (Limits == null)
+        {
+            Limits = GetComponent<Image>();
+        }
+
+        RectTransform rt = Limits.rectTransform;
+        Vector3[] corners = new Vector3[4];
+        rt.GetWorldCorners(corners);
+
+        minX = corners[0].x;
+        maxX = corners[0].x;
+        minY = corners[0].y;
+        maxY = corners[0].y;
+
+        for (int i = 1; i < corners.Length; i++)
+        {
+            if (corners[i].x < minX) minX = corners[i].x;
+            if (corners[i].x > maxX) maxX = corners[i].x;
+            if (corners[i].y < minY) minY = corners[i].y;
+            if (corners[i].y > maxY) maxY = corners[i].y;
+        }
+
+        Debug.Log($"RectTransform World Corners - MinX: {minX}, MaxX: {maxX}, MinY: {minY}, MaxY: {maxY}");
+    }
 }
